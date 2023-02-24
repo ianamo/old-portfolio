741f1827c3701ac964b28662173e8edb881fa74c
Ian Mosley <iananmo86@gmail.com>
2023-02-24 14:52:06 -0600

    refactored Treble Tutor

--- treble-tutor/index.js
+++ treble-tutor/index.js
@@ -1,29 +1,31 @@
-var showNote = false;
-var lastNote = null;
+let showNote = false;
+let lastNote = null;
+
+function playNote(noteStr) {
+	const note = new Audio("sounds/"+noteStr+'.mp3');
+	note.play();
+}
 
 $('.key').click(function () 
 	{ 
-		if (this.value == 'c' && lastNote != 'c-note') {
+		const thisNote = this.value + '-note';
+		
+		if ((thisNote == 'c-note' || lastNote=='c-note')&&(thisNote!==lastNote))  { // if we're playing the c note -- or we have played it, and we're playing something else -- we need to make the little line appear or go away
 			$('.note-line').toggleClass('hide');
-		} else if (this.value != 'c' && lastNote == 'c-note') {
-			$('.note-line').toggleClass('hide');	
-		}
+		} 
 
-		if (showNote == false) {
+		if (!showNote) { // note div remains hidden until first note is played
 			$('.note').toggleClass("hide");
 			showNote = true;
 		}
-		if (lastNote == null) {
-			lastNote = this.value + '-note';
-			$('.note').toggleClass(this.value+'-note');
-			var note = new Audio("sounds/"+this.value+'-note.mp3');
-			note.play();
-		} else {
-			$('.note').toggleClass(lastNote);
-			$('.note').toggleClass(this.value+'-note');
-			note = new Audio("sounds/"+this.value+'-note.mp3');
-			note.play();
-			lastNote = this.value+'-note';
-		}
-		
+
+		lastNote && $('.note').toggleClass(lastNote); // if there is a last note, make it go away 
+		$('.note').toggleClass(thisNote);
+		playNote(thisNote);
+		lastNote = thisNote;
 });
+
+
+//
+//
+ 
